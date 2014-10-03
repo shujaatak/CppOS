16e8c8fa498942d03443af64dec1314ebba9e2d9
zchrissirhcz <zchrissirhcz@gmail.com>
2014-10-04 00:51:48 +0800

    在Meaty_Skeleton基础上导入C++支持

--- README.md
+++ README.md
@@ -2,3 +2,27 @@ CppOS
 =====
 
 A hobby OS written in C++
+
+### 开发目的
+1.加深进程管理、内存管理、文件系统的理解
+2.开发一套图形界面玩玩
+3.尝试添加网络功能
+
+
+### 开发环境
+#### 系统
+Fedora20, 或其他Linux发行版
+
+#### 工具
+交叉编译器 i686-elf-gcc等
+引导程序 grub2
+汇编器 nasm
+模拟器 qemu
+当然还有make
+
+### 参考资料
+[osdev.org](http://osdev.org)
+[How-to-Make-a-Computer-Operating-System](https://github.com/SamyPesse/How-to-Make-a-Computer-Operating-System)
+[30天自制操作系统](http://book.douban.com/subject/11530329/)
+
+
---
+++ sysroot/kernel/kernel/kernel.c
@@ -0,0 +1,45 @@
+/*
+ * ==================================================
+ *
+ *       Filename:  kernel.c
+ *
+ *    Description:  终于到内核了!!
+ *
+ *        Version:  0.01
+ *        Created:  2014年10月01日 星期三 14时16分32秒
+ *         Author:  ChrisZZ, zchrissirhcz@163.com
+ *        Company:  ZJUT
+ *
+ * ==================================================
+ */
+
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+
+#include <kernel/tty.h>
+
+extern "C" {
+
+class Display{
+public:
+	void hoho(){
+		printf("Hello, Chrisssdsfdsfsdfsdf");
+	}
+};
+
+void kernel_early(void){
+	terminal_initialize();
+}
+
+void kernel_main(void){
+	while(true){
+
+		printf("Hello, kernel World!");
+	}
+	//dis.hoho();
+}
+
+
+}
